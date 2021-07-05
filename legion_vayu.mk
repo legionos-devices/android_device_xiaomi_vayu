37061d6884e7472f322df728f112fdc648af8acb
Christian de la Cruz <chrisdlc119@outlook.com>
2021-07-04 21:32:31 -0500

    vayu: Bringup Legion OS

--- legion_vayu.mk
+++ legion_vayu.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2018-2021 AOSP
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common Legion OS configurations
+$(call inherit-product, vendor/legion/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := legion_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -24,7 +25,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="vayu"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-DEVICE_MAINTAINER := kubersharma001
-TARGET_INCLUDE_PIXEL_CHARGER := true
-
